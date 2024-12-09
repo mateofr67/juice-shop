9c8e80e85d6266ce6af362d74ec5f1d08bd544f4
mateofr67 <145790812+mateofr67@users.noreply.github.com>
2024-12-09 03:16:47 +0100

    Update redirect.ts

--- routes/redirect.ts
+++ routes/redirect.ts
@@ -10,19 +10,39 @@ import { challenges } from '../data/datacache'
 
 const security = require('../lib/insecurity')
 
-module.exports = function performRedirect () {
+module.exports = function performRedirect() {
   return ({ query }: Request, res: Response, next: NextFunction) => {
-    const toUrl: string = query.to as string
-    if (security.isRedirectAllowed(toUrl)) {
-      challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => { return toUrl === 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' || toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' || toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6' })
-      challengeUtils.solveIf(challenges.redirectChallenge, () => { return isUnintendedRedirect(toUrl) })
-      res.redirect(toUrl)
+    const toUrl: string = query.to as string;
+
+    // Definir los dominios permitidos para la redirección
+    const allowedDomains = [
+      "https://explorer.dash.org",
+      "https://blockchain.info",
+      "https://etherscan.io"
+    ];
+
+    // Comprobar si la URL proporcionada comienza con uno de los dominios permitidos
+    const isAllowedDomain = allowedDomains.some(domain => toUrl.startsWith(domain));
+
+    if (isAllowedDomain) {
+      // Realizar la redirección si la URL es válida
+      challengeUtils.solveIf(challenges.redirectCryptoCurrencyChallenge, () => {
+        return toUrl === 'https://explorer.dash.org/address/Xr556RzuwX6hg5EGpkybbv5RanJoZN17kW' || 
+               toUrl === 'https://blockchain.info/address/1AbKfgvw9psQ41NbLi8kufDQTezwG8DRZm' || 
+               toUrl === 'https://etherscan.io/address/0x0f933ab9fcaaa782d0279c300d73750e1311eae6';
+      });
+      challengeUtils.solveIf(challenges.redirectChallenge, () => {
+        return isUnintendedRedirect(toUrl);
+      });
+      res.redirect(toUrl);
     } else {
-      res.status(406)
-      next(new Error('Unrecognized target URL for redirect: ' + toUrl))
+      // Si no es un dominio permitido, enviar un error 406 (Not Acceptable)
+      res.status(406).json({
+        error: 'Unrecognized target URL for redirect: ' + toUrl
+      });
     }
   }
-}
+};
 
 function isUnintendedRedirect (toUrl: string) {
   let unintended = true
